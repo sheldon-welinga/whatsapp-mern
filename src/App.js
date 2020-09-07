a62d21fcbbba2faf0144dcc24dac8275260b7261
Sheldon Welinga <sheldonwelinga@gmail.com>
2020-09-07 18:00:56 +0300

    updated the client to receive messages from the database

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Pusher from "pusher-js";
 import "./App.css";
 import Sidebar from "./components/Sidebar";
 import Chat from "./components/Chat";
+import axiosInstance from "./axiosInstance";
 
-function App() {
+const App = () => {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    axiosInstance
+      .get("/api/v1/messages/sync")
+      .then((response) => {
+        setMessages(response.data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
+
+  useEffect(() => {
+    //run the pusher function on load and refresh all messages
+    const pusher = new Pusher("660a4d2bd5ea7541559c", {
+      cluster: "mt1",
+    });
+
+    const channel = pusher.subscribe("message");
+    channel.bind("inserted", (newMessage) => {
+      // alert(JSON.stringify(newMessage));
+      setMessages([...messages, newMessage]);
+    });
+
+    return () => {
+      channel.unbind_all();
+      channel.unsubscribe();
+    };
+  }, [messages]);
+
+  console.log(messages);
   return (
     <div className="app">
       <div className="app-body">
@@ -12,6 +46,6 @@ function App() {
       </div>
     </div>
   );
-}
+};
 
 export default App;
